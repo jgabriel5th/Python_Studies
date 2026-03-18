824ec03bc76d16d200a4de4443b8e3790673480d
JG5th <joaojklgabriel@gmail.com>
2026-03-18 10:45:21 -0300

    Updated README, learned more about types of Errors

--- Exception Handling/exception_handling.py
+++ Exception Handling/exception_handling.py
@@ -1,1 +1,11 @@
-'''Exception Handling'''
+'''
+Exception Handling: it's when the code is written right, but the user finds a way to break it.
+As my professor says: "Don't trust the users, they'll always find a way to break the code."
+
+Error: programming error is mainly known as bugs and the process to remove bugs from the program is called debugging.
+Types of Errors:
+Syntax Errors - Error in the source code of a program, any aspects of the code that do not conform to the syntax of the
+chosen programming language will generate a syntax error. Also called compilation errors.
+Logic Errors - They usually occur due to improper logic used in the program. They are difficult to debug.
+Runtime Errors - Syntax and Logic are right, but for example: dividing any number by zero will result in a runtime error.
+'''
