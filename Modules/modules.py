74793f3b41809af8f381af4aa8c31be0a6aa97c2
JG5th <joaojklgabriel@gmail.com>
2026-03-21 11:08:54 -0300

    Learned more about modules and their concepts

---
+++ Modules/modules.py
@@ -0,0 +1,9 @@
+'''
+Reusability: reuse a particular code than developing a new one.
+Python modules are used to do that.
+
+Modules refer to a file containing Python statements and definitions.
+Modules are also used to break down large programs into small manageable and organized files.
+It's possible to define the most used functions in a module and import it, instead of copying them into different programs.
+Examples of modules: Math and Random.
+'''
