745789b4c33f5ec0067ad8dc4d96b1da0b855afd
JG5th <joaojklgabriel@gmail.com>
2026-03-21 16:15:18 -0300

    Learned about important built-in modules and created a custom module

---
+++ Modules/calculator.py
@@ -0,0 +1,17 @@
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    if n1 > n2:
+        return n1 - n2
+    else:
+        return n2 - n1
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n1 > n2:
+        return n1 / n2
+    else:
+        return n2 / n1
--- Modules/modules.py
+++ Modules/modules.py
@@ -9,7 +9,7 @@ Examples of modules: Math and Random.
 
 Types of Modules:
 Built-in modules - They're available in Python by default, developed and maintained by the Python developer team.
-It's possible to directly import them into to file, example: Math module.
+It's possible to directly import them into to file, example: Math module. There are more in Python Module Index.
 
 Third-party modules -  They're similar to built-in modules, but they've got to be installed first. These modules can be installed using the 'pip' command.
 PIP is a package manager for Python packages. They're uploaded and stored at the PyPI repository. PyPI = Python Package Index.
@@ -26,7 +26,7 @@ The three ways of using a Python module:
 
 The import statement - To use a module, the first thing to do is import it using the import keyword, doing it will import the whole module into the Python project.
 To use the function provided by the imported module it'll be necessary to use the dot(.) operator. 
-Example: module_name.functionName() <- That's how an imported function will work.
+Example: module_name.functionName() <- That's how an imported function will work. It serves for built-in, third-party and custom modules.
 Syntax to import: import module_name
 
 Import statement and renaming - There are situations when the module's name is big, in order to solver this issue it's possible to rename a module while importing it.
@@ -49,3 +49,27 @@ print(m.sqrt(4))
 
 from math import floor
 print(floor(3.2))
+
+'''
+Some important built-in Modules
+- Math Module: mathematical functions defined by the C standard.
+- Random Module: pseudo-random number generators for various distributions.
+- Statistics Module: functions for calculating mathematical statistics of numeric real-valued data.
+- Array Module: similar to a list but constrained which means that an array containing string values won't accept a numeric value to be stored in it.
+- Datetime Module: supplies classes for manipulating dates and times.
+- Time Module: provides various time-related functions.
+- SQLite3 Module - it is a C library that provides a database that does not requite a separate server process and allows accessing the database.
+- Urllib Module - a package which collects a bunch of modules for working with URLs.
+- Tkinter Module - the standard Python interface to the Tk GUI tookit.
+'''
+
+# Importing a custom module
+import calculator
+
+# Using calculator
+a = int(input("Insert the first number: "))
+b = int(input("Insert the second number: "))
+print(f'''Addition is {calculator.add(a, b)}
+Subtraction is {calculator.sub(a, b)}
+Multiplication is {calculator.multiply(a, b)}
+Division is {calculator.divide(a, b)}''')
