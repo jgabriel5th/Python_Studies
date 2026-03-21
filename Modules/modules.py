ed43fb68526b9f78f4d8110806039cffab9fb1a3
JG5th <joaojklgabriel@gmail.com>
2026-03-21 11:29:26 -0300

    Learned about types of modules

--- Modules/modules.py
+++ Modules/modules.py
@@ -6,4 +6,18 @@ Modules refer to a file containing Python statements and definitions.
 Modules are also used to break down large programs into small manageable and organized files.
 It's possible to define the most used functions in a module and import it, instead of copying them into different programs.
 Examples of modules: Math and Random.
+
+Types of Modules:
+Built-in modules - They're available in Python by default, developed and maintained by the Python developer team.
+It's possible to directly import them into to file, example: Math module.
+
+Third-party modules -  They're similar to built-in modules, but they've got to be installed first. These modules can be installed using the 'pip' command.
+PIP is a package manager for Python packages. They're uploaded and stored at the PyPI repository. PyPI = Python Package Index.
+
+Syntax to download the third-party modules in the command-line/terminal.
+Linux: pip3 install module_name
+Windows: pip install module_name
+
+Custom modules - They're the packages created by the developer, basically they are Python files which can be imported into another Python file/project.
+When imported, it's possible to access their attributes and methods. These files files are called Python custom modules.
 '''
