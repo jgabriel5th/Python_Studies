cc8428bd4833120300fbd02ca6d8735449b52f1b
JG5th <joaojklgabriel@gmail.com>
2026-03-22 22:05:05 -0300

    Started the studies about Object-Oriented Programming in Python

---
+++ Object-Oriented Programming/oop.py
@@ -0,0 +1,24 @@
+'''
+Object-Oriented Programming:
+It is one of the paradigms or patterns in programming, paradigms define how to write and structure the code.
+Each paradigm has different functionalities and behaviors.
+
+Examples of programming patterns:
+- Structural programming
+- Procedural programming
+- Object-oriented programming
+- Functional programming
+- And there are more...
+'''
+# Python is an object-oriented programming language, and here's an example with type() function:
+name = "João"
+print(type(name)) # it returns <class 'str'>
+
+'''
+Classes and Objects:
+- OOP is a paradigm that provides a means for structuring programs so that properties and behaviors are bundled into indivual objects.
+- The two main concepts about OOP are Classes and Objects, in other words the variable 'name' is the object of the class 'string' at the most basic level.
+- It allows developers to develop applications using the OOPs approach with a major focus on code reusability.
+- Object-oriented programming is an approach for modeling concrete, real-world things, and relations between those things.
+- OOP models real-world entities as programming objects that have some data associated with them and can perform certain functions.
+'''
