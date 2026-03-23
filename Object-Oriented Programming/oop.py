e698ad368f62497cd6e9b48a37d9553a72524ade
JG5th <joaojklgabriel@gmail.com>
2026-03-23 12:57:41 -0300

    Studied about the 4 Pillars of OOP

--- Object-Oriented Programming/oop.py
+++ Object-Oriented Programming/oop.py
@@ -21,4 +21,13 @@ Classes and Objects:
 - It allows developers to develop applications using the OOPs approach with a major focus on code reusability.
 - Object-oriented programming is an approach for modeling concrete, real-world things, and relations between those things.
 - OOP models real-world entities as programming objects that have some data associated with them and can perform certain functions.
+
+The 4 Pillars of OOP:
+Abstraction - It means to abstract only the details/data which might concern the user. The developer let the user abstract only the details that are required, the rest is hidden.
+Encapsulation - It tries to encapsulate all the data as a single entity. Similarly a black box, where an input is passed, the box processes it and gives the result as an output
+from the other end, therefore what happens in the black box is totally hidden. Encapsulation also hides the data.
+Inheritance - A class can inherit various characteristics and capabilities from another class. It supports the concept of reusability, when a new a feature includes part from an
+existing code, it can be derived from the existing one instead of developing from scratch.
+Polymorphism - This word means "having many forms", so polymorphism is the ability of something to have more than one form. In programming, an operation may exhibit different behaviors
+in different instances, these behaviors will depend upon the types of data used in the operation.
 '''
