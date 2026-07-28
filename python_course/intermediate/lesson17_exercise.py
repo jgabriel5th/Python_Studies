6676447346bf78a5a5946505bd699d002776a68b
jgabriel5th <joaojklgabriel@gmail.com>
2026-07-28 14:51:53 -0300

    Exercise refactored

--- python_course/intermediate/lesson17_exercise.py
+++ python_course/intermediate/lesson17_exercise.py
@@ -21,19 +21,30 @@ right_answers = 0
 for option in range(3):
     question_name = question[option]['Question']
     print(question_name)
+
     options = question[option]['Options']
     right_answer = question[option]['Answer']
+
     for index, name in enumerate(options):
         print(f'{index}) {name}')
     answer = input('Type the right answer: ')
-    if answer == right_answer:
-        right_answers += 1
-        print(f'Right answer! ✅{right_answers}/3')
-    elif answer != right_answer:
-        print(f'Wrong answer ❌{right_answers}/3')
-    else:
-        print('You typed an invalid value.')
+    got_it = False
+    answer_int = None
+    qtd_options = len(options)
 
+    if answer.isdigit():
+        answer_int = int(answer)
+
+    if answer_int is not None:
+        if answer_int >= 0 and answer_int < qtd_options:
+            if options[answer_int] == right_answer:
+                got_it = True
+
+    if got_it:
+        right_answers += 1
+        print('Right answer✅')
+    else:
+        print('Wrong answer❌')
 def answerAnalyzer(x):
     if x == 3:
         return f'You got 100% of the answers right'
@@ -45,5 +56,5 @@ def answerAnalyzer(x):
         return f'You got {(0.33 * x)}% of the answer right'
     else:
         return 'You got 0% of the test right.'
-
+print(f'You got {right_answers} of {len(question)} right')
 print(answerAnalyzer(right_answers))
