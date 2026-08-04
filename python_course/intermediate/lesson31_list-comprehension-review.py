e00f0941acea32be3107c40bf5a606c483ed76c9
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-04 14:57:50 -0300

    Reviewing list comprehension

---
+++ python_course/intermediate/lesson31_list-comprehension-review.py
@@ -0,0 +1,55 @@
+# SYNTAX:
+numbers = [1, 2, 3, 4, 5]
+new_numbers = [number for number in numbers]
+
+# new_numbers = []
+# for number in numbers:
+#     new_numbers.append(number)
+print(numbers)
+print(new_numbers)
+
+##############################
+# UTILIZATION:
+def divisionFn(x, y):
+    return x / y
+
+def multiplicationFn(x, y):
+    return x * y
+
+def exponentiation(x, y):
+    return x ** y
+
+# division = [number / 2 for number in numbers] # Map
+# multiplication = [number * 2 for number in numbers]
+# square = [number ** 2 for number in numbers]
+
+division = [divisionFn(number, 2) for number in numbers] # Using functions
+multiplication = [multiplicationFn(number, 2) for number in numbers]
+square = [exponentiation(number, 2) for number in numbers]
+
+print(division)
+print(multiplication)
+print(square)
+
+##############################
+# FILTER:
+numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_numbers = [number for number in numbers2 if number > 5]
+odd_numbers = [number for number in numbers2 if number % 2 != 0]
+even_numbers = [number for number in numbers2 if number % 2 == 0]
+another_if = [number if number != 6 else 600 for number in even_numbers]
+
+print(numbers2)
+print(new_numbers)
+print(odd_numbers)
+print(even_numbers)
+print(another_if)
+
+##############################
+# ROWS AND COLUMNS:
+# for x in range(1, 11):
+#     for y in range(1, 6):
+#         print(x, y)
+
+rows_columns = [(x, y) if y != 2 else (x, y * 1000) for x in range(1, 11) for y in range(1, 6) if x != 2]
+print(rows_columns)
