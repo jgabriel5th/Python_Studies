a2250534a4f7c0161e589b7a662647f82b29e43d
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-26 17:22:02 -0300

    Studied about count from itertools

---
+++ python_course/intermediate/lesson60_count.py
@@ -0,0 +1,21 @@
+# count is an endless iterator
+from itertools import count
+
+c1 = count(step=10, start=10)
+r1 = range(10, 100, 2)
+
+print('c1', hasattr(c1, '__iter__'))
+print('c1', hasattr(c1, '__next__'))
+print('r1', hasattr(r1, '__iter__'))
+print('r1', hasattr(r1, '__next__'))
+
+print('count')
+for i in c1:
+    if i > 100:
+        break
+
+    print(i)
+print()
+print('range')
+for i in r1:
+    print(i)
