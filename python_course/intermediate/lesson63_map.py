fcb90b255443382231717428bc96d05aecb2abd5
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-29 20:44:37 -0300

    Studied about map function, generator type and partial

--- python_course/intermediate/lesson63_map.py
+++ python_course/intermediate/lesson63_map.py
@@ -1,3 +1,5 @@
+from functools import partial
+from types import GeneratorType
 # map - to map data
 def print_iter(iterator):
     print(*list(iterator), sep='\n')
@@ -10,11 +12,32 @@ products = [
     {'name': 'Product 2', 'price': 105.87},
     {'name': 'Product 4', 'price': 69.90},
 ]
-def order_products(product):
-    return product['name']
 
-ordered_products = sorted(products, key=order_products)
-new_products = [ # Stopped my study here
-    {**p} for p in products
-]
+# def increase_percentage(percentage): # Usually a closure would be made to create ways to make new functions.
+#     def take_value(value):
+#         return round(value * percentage, 2)
+#     return take_value
+# increase_ten_percent = increase_percentage(1.1)
+def increase_percentage(value, percentage):
+    return round(value * percentage, 2)
+increase_ten_percent = partial(increase_percentage, percentage=1.1) # But will partial from functools, it's possible to create it.
+
+# new_products = [
+#     {**p, 'price': increase_ten_percent(p['price'])} for p in products
+# ]
+
+def changeProductPrice(product):
+    return {**product, 'price': increase_ten_percent(product['price'])}
+
+new_products = map(changeProductPrice, products)
 print_iter(new_products)
+print(hasattr(new_products,'__iter__'))
+print(hasattr(new_products,'__next__')) # It's an iterator
+print(isinstance(new_products, GeneratorType)) # But not a generator
+
+print(
+    list(map( # If not list is not put, it'll print a map object(because it's an iterator)
+        lambda x: x * 3,
+        [1, 2, 3, 4]
+    )
+))
