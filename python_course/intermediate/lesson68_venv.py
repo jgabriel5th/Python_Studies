f94dfb1092cd8b29cb2ff31ffb18f4714478208e
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-30 21:29:07 -0300

    Studied about virtual environments in Python

---
+++ python_course/intermediate/lesson68_venv.py
@@ -0,0 +1,7 @@
+# Virtual environments in Python (venv)
+# A virtual environment loads all your Python installation 
+# into a folder in a chosen path.
+# When activating a virtual environment, the venv installation will be used.
+# Venv is a module that'll be used to create virtual environments.
+# You can give the name you prefer to a virtual environment, but the most commons ones are:
+# venv env .venv .env
