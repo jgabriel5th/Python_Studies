c6900832d315943e3a421dc796ee09fca53322aa
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-30 22:29:22 -0300

    Studied how to create a venv and activate it

--- python_course/intermediate/lesson68_venv.py
+++ python_course/intermediate/lesson68_venv.py
@@ -5,3 +5,10 @@
 # Venv is a module that'll be used to create virtual environments.
 # You can give the name you prefer to a virtual environment, but the most commons ones are:
 # venv env .venv .env
+
+# How to create a venv:
+# python -m venv venv(virtual environment name)
+
+# How to activate and deactivate venv:
+# . venv/bin/activate or source ven/bin/activate # To activate
+#  deactivate # To deactivate
