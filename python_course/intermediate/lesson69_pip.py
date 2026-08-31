64d29ff3a759be9ef5ac499d7b578a363ac8f30a
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-30 22:52:44 -0300

    Studied about pip

---
+++ python_course/intermediate/lesson69_pip.py
@@ -0,0 +1,5 @@
+# pip is used to install modules from PyPi
+# How to install with pip: pip install module_name
+# How to uninstall with pip: pip uninstall module_name
+# How to see available module versions: pip index versions module_name
+# How to see what is installed: pip freeze
