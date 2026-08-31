f64b1943475553c1c4ef43d80360e3117186c4fa
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-30 21:13:51 -0300

    Studied about recursive function limits and cautions

---
+++ python_course/intermediate/lesson67_recursive-function2.py
@@ -0,0 +1,32 @@
+# Recursive function and recursivity
+# - functions that can call themselves
+# - useful to share big problems in smaller parts
+# Every recursive function must have:
+# - A problem that can be split in smaller parts
+# - A recursive case that solves the small problem
+# - A base case that stops the recursion
+# - factorial - n! = 5 * 4 * 3 * 2 * 1 = 120
+# https://brasilescola.uol.com.br/matematica/fatorial.htm
+
+# import sys
+# sys.setrecursionlimit(1004) # Not recommended
+
+# def recursive(begin=0, end=10):
+#     print(begin, end)
+#     # Base case
+#     if begin >= end:
+#         return end
+    
+#     # Recursive case
+#     # count till reach the end
+#     begin += 1
+#     return recursive(begin, end)
+
+# print(recursive())
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+print(factorial(5))
