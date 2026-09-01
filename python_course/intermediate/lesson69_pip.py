739b8f569769e588ec123f659b90a30a9ee41037
jgabriel5th <joaojklgabriel@gmail.com>
2026-08-31 22:06:04 -0300

    Studied about requirements and how to manage it

--- python_course/intermediate/lesson69_pip.py
+++ python_course/intermediate/lesson69_pip.py
@@ -3,3 +3,5 @@
 # How to uninstall with pip: pip uninstall module_name
 # How to see available module versions: pip index versions module_name
 # How to see what is installed: pip freeze
+# How to create requirements or update it: pip freeze > requirements.txt
+# How to install from requirements.txt: pip install -r .\requirements.txt
